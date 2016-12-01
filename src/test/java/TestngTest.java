3b98a5f7368b1f23535174e82df2337f49c0d99f
Gravituss <zaretskyyy@gmail.com>
2016-12-01 03:23:40 +0200

    2 testng tests

--- src/test/java/TestngTest.java
+++ src/test/java/TestngTest.java
@@ -16,7 +16,34 @@ public class TestngTest extends BaseJdbcTest {
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             stmt = conn.createStatement();
 
-            System.out.println(stmt.executeUpdate("delete from spaceships where speed > 0"));
+            System.out.println(stmt.executeUpdate("insert into spaceships values ('starone', 45, 15 )"));
+            System.out.println(stmt.executeUpdate("insert into spaceships values ('stargalaxy', 55, 58 )"));
+            System.out.println(stmt.executeUpdate("insert into spaceships values ('staruniverse', 177, 133 )"));
+
+            readTable();
+
+            System.out.println("-----------Cleaning up the table...");
+            System.out.println(stmt.executeUpdate("delete from spaceships"));
+            readTable();
+
+//            STEP 6: Clean-up environment
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+        } catch (Exception e) {
+            System.out.println("Exception e");
+        }
+    }
+    @Test
+    public void test2() {
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+
             System.out.println(stmt.executeUpdate("insert into spaceships values ('starone', 45, 15 )"));
             System.out.println(stmt.executeUpdate("insert into spaceships values ('stargalaxy', 55, 58 )"));
             System.out.println(stmt.executeUpdate("insert into spaceships values ('staruniverse', 177, 133 )"));
@@ -39,15 +66,5 @@ public class TestngTest extends BaseJdbcTest {
             System.out.println("Exception e");
         }
     }
-    @Test
-    public void test2() {
-
-    }
-    @Test
-    public void test3() {
-
-    }
-
-
 
 }
